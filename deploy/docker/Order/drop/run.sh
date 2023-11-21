3da5474ee7072b856a506937f3387e168845a833
simina3 <SiminaDaniela.Ciui@alu.uclm.es>
2023-11-21 15:33:04 +0100

    Dockerizando y subiendo imágenes

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,13 @@
+# a ejecutar al iniciar un contenedor del servicio de gestión de órdenes
 #!/bin/bash
 
+# Espera hasta que MongoDB esté disponible antes de continuar
 while ! curl http://mongo:27017/
 do
-  echo "$(date) - still trying"
+  echo "$(date) - Still trying to connect to MongoDB"
   sleep 1
 done
-echo "$(date) - connected successfully"
+echo "$(date) - Connected to MongoDB successfully"
 
-java -jar ordering-*.jar
+# Ejecuta el servicio de órdenes
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties
